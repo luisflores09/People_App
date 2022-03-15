193beaaf2069d070c65a3324298b953e26fe15e8
Luis <iluisflores@yahoo.com>
2022-03-15 12:12:06 -0500

    "Database Connection"

--- server.js
+++ server.js
@@ -4,11 +4,26 @@
 // get .env variables
 require("dotenv").config();
 // pull PORT from .env, give default value of 3001
-const { PORT = 3001 } = process.env;
+// pull DATABASE_URL from .env
+const { PORT = 3001, DATABASE_URL } = process.env;
 // import express
 const express = require("express");
 // create application object
 const app = express();
+// import mongoose
+const mongoose = require('mongoose');
+
+///////////////////////////////
+// DATABASE CONNECTION
+////////////////////////////////
+// Establish Connection
+mongoose.connect(DATABASE_URL);
+// Connection Events
+mongoose.connection
+  .on("open", () => console.log("You are connected to MongoDB"))
+  .on("close", () => console.log("You are disconnected from MongoDB"))
+  .on("error", (error) => console.log(error))
+
 
 ///////////////////////////////
 // ROUTES
