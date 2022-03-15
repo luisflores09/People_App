ce20c4b83c69b3e6091d263ec2042036bb939368
Luis <iluisflores@yahoo.com>
2022-03-15 12:08:06 -0500

    "Starting Server.js"

---
+++ server.js
@@ -0,0 +1,24 @@
+///////////////////////////////
+// DEPENDENCIES
+////////////////////////////////
+// get .env variables
+require("dotenv").config();
+// pull PORT from .env, give default value of 3001
+const { PORT = 3001 } = process.env;
+// import express
+const express = require("express");
+// create application object
+const app = express();
+
+///////////////////////////////
+// ROUTES
+////////////////////////////////
+// create a test route
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+///////////////////////////////
+// LISTENER
+////////////////////////////////
+app.listen(PORT, () => console.log(`listening on PORT ${PORT}`));
